a6c41cddaadfa93319bae0a1ebb4938870f6c859
azazeldev <68147363+azazelcodes@users.noreply.github.com>
2026-01-17 23:24:54 +0100

    baseurl test

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://azazelcodes.github.io/uawiki/",
+    baseUrl: "azazelcodes.github.io/uawiki",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
--- quartz/cli/args.js
+++ quartz/cli/args.js
@@ -78,7 +78,7 @@ export const BuildArgv = {
   },
   baseDir: {
     string: true,
-    default: "test",
+    default: "",
     describe: "base path to serve your local server on",
   },
   port: {
